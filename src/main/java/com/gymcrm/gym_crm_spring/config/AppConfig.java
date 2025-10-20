d56c612316ee019a6ab46842f8a359b0637aa03a
Baxtiyar <baxtiyar_bobokulov@epam.com>
2025-10-20 20:52:20 +0300

    fix: import fixed in AppConfig

--- src/main/java/com/gymcrm/gym_crm_spring/config/AppConfig.java
+++ src/main/java/com/gymcrm/gym_crm_spring/config/AppConfig.java
@@ -1,12 +1,10 @@
 package com.gymcrm.gym_crm_spring.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-@EnableAspectJAutoProxy
 public class AppConfig {
 
     @Bean
